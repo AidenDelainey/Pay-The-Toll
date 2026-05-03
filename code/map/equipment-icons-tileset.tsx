2c935a908204119be5dc2d30e6d0126802ca0d7a
AidenDelainey <aiden.delainey@lskysd.ca>
2026-05-03 14:20:28 -0600

    Add files via upload

---
+++ code/map/equipment-icons-tileset.tsx
@@ -0,0 +1,49 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.0" name="equipment-icons-tileset" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../image/tilesets_edit/equipment-icons-tileset.png" width="512" height="512"/>
+ <tile id="36">
+  <properties>
+   <property name="item" value="metal_fisters"/>
+  </properties>
+ </tile>
+ <tile id="53">
+  <properties>
+   <property name="item" value="wooden_staff"/>
+  </properties>
+ </tile>
+ <tile id="55">
+  <properties>
+   <property name="item" value="wooden_wand"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="item" value="flimsy_dagger"/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="item" value="wing_sword"/>
+  </properties>
+ </tile>
+ <tile id="125">
+  <properties>
+   <property name="item" value="enchanted_drums"/>
+  </properties>
+ </tile>
+ <tile id="146">
+  <properties>
+   <property name="item" value="silver_ring"/>
+  </properties>
+ </tile>
+ <tile id="150">
+  <properties>
+   <property name="item" value="apprentice_cloak"/>
+  </properties>
+ </tile>
+ <tile id="179">
+  <properties>
+   <property name="item" value="simple_bracers"/>
+  </properties>
+ </tile>
+</tileset>
