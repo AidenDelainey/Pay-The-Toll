95edb7509c1fc5d77fe80a46053e4715cebc3fda
AidenDelainey <aiden.delainey@lskysd.ca>
2026-05-28 09:05:34 -0600

    Delete code/map/walls.tsx

--- code/map/walls.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="walls" tilewidth="32" tileheight="32" tilecount="144" columns="12">
- <image source="../image/tilesets_edit/4_buildings.png" width="384" height="384"/>
-</tileset>
