950a24477fdff28bcf34e6269f39c3f5c729a332
AidenDelainey <aiden.delainey@lskysd.ca>
2026-05-28 09:05:11 -0600

    Delete code/map/test.tsx

--- code/map/test.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.12.0" name="test" tilewidth="32" tileheight="32" tilecount="112" columns="14">
- <image source="../image/tilesets_edit/8_cave.png" width="448" height="256"/>
-</tileset>
