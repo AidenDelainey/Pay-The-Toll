a6b0212b374011cbce96e546549b0f10f15a0f83
AidenDelainey <aiden.delainey@lskysd.ca>
2026-06-08 10:02:54 -0600

    Add files via upload

--- code/main.py
+++ code/main.py
@@ -12,6 +12,12 @@ pygame.display.set_mode((WIDTH, HEIGHT)) # <-- don't remove or move
 from level import Level
 
 icon_img = pygame.image.load(os.path.join(enemies_path, "gnome", "idle.png" )).convert_alpha()
+LEVELS = ["../map/forest map 1.tmx",
+          "../map/forest map 2.tmx",
+          "../map/castle map 1.tmx",
+          "../map/castle map 2.tmx"
+          ]
+
 
 class Game:
     def __init__(self):
@@ -25,7 +31,17 @@ class Game:
         pygame.display.set_icon(icon_img)
         
         self.clock = pygame.time.Clock()
-        self.level = Level()
+        
+        self.current_level = 0
+        self.level = Level(LEVELS[self.current_level])
+        
+    def next_level(self):
+        self.current_level += 1
+        
+        if self.current_level >= len(LEVELS):
+            self.current_level = 0
+            
+        self.level.load_level(LEVELS[self.current_level])
         
     def run(self):
         while True:
@@ -34,11 +50,18 @@ class Game:
                 if event.type == pygame.QUIT:
                     pygame.quit()
                     sys.exit()
+                    
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_n:
+                        self.next_level()
         
                 self.level.handle_input(event)
                 
             #self.screen.fill('black')
             self.level.run()
+            
+            if self.level.finished_level:
+                self.next_level()
             
             fps = self.clock.get_fps()
             fps_text = self.font.render(f'FPS: {int(fps)}', True, (255, 255, 255))
